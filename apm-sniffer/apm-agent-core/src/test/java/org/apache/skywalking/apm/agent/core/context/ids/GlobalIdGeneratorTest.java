f79b81494b1440e87f319fa80b96d78958495142
geekymv <ym2011678@foxmail.com>
2022-06-07 11:36:00 +0800

    trace id

--- apm-sniffer/apm-agent-core/src/test/java/org/apache/skywalking/apm/agent/core/context/ids/GlobalIdGeneratorTest.java
+++ apm-sniffer/apm-agent-core/src/test/java/org/apache/skywalking/apm/agent/core/context/ids/GlobalIdGeneratorTest.java
@@ -2,13 +2,30 @@ package org.apache.skywalking.apm.agent.core.context.ids;
 
 import org.junit.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class GlobalIdGeneratorTest {
 
     @Test
     public void generate() {
-        for(int i = 0; i < 10001; i++) {
+
+        new Thread(()-> {
+            GlobalIdGenerator.generate();
+        }).start();
+
+        new Thread(()-> {
+            GlobalIdGenerator.generate();
+        }).start();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+      /*  for(int i = 0; i < 10001; i++) {
             String id = GlobalIdGenerator.generate();
             System.out.println(id);
-        }
+        }*/
     }
 }
